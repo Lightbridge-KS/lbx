793df6e080264c40a98bc8c69d6771dcf09afaff
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-07-26 17:09:22 +0700

    Add helper print_messages()

---
+++ R/helper_cnd.R
@@ -0,0 +1,31 @@
+#' Print Character Vector to Messages
+#'
+#'
+#' @param x A character vector
+#' @param sep Separator between each character elements
+#' @param prefix Prefix of each character elements
+#' @param suffix Suffix of each character elements
+#'
+#' @return A message
+#' @export
+#'
+#' @examples
+#' chr <- c("a","b","c")
+#' print_messages(chr)
+print_messages <- function(x, sep = "\n", prefix = "", suffix = "") {
+
+  pad_len <- length(x) - 1
+
+  # Add optional prefix or suffix
+  if (prefix != "" || suffix != "") {
+    for (i in seq_along(x)) {
+      x[i] <- paste0(prefix, x[i], suffix)
+    }
+  }
+  # Add Separator
+  for (i in 1:pad_len) {
+    x[i] <- paste0(x[i], .sep = sep)
+  }
+
+  message(x)
+}
