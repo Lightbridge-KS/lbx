e1c1cf4b8b50b4083cb990de45914a1aafd357ad
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-07-26 15:06:41 +0700

    Add count_vars_plot and count_vars_plot_facet

--- R/count.R
+++ R/count.R
@@ -54,3 +54,112 @@ count_var_table <- function(x,
 
 }
 
+# Count variables for Plotting Easier --------------------------------------
+
+
+#' Count variable for plotting
+#'
+#' This function count variables, do `fct_reorder` by that count, extract percentage and construct `labels` column
+#' ready to be plotted.
+#'
+#' @param x A data.frame
+#' @param ... (Quoted or Unquoted) Variables to count
+#' @param var_desc If `TRUE` levels of the variable will be reorder in descending order according to
+#'   count of that variable
+#' @param label_number Construction of `labels` column in the output
+#' * "percent": use `%` (eg. 60%)
+#' * "count": count as integer (eg. 6)
+#' * "both": use `%` and count in parenthesis  (eg. 60% (6))
+#' @param lump_prop Proportion that lump levels to "Other" (passed to `fct_lump` at `prop` argument)
+#' @param other_level Name of the "Other" level
+#'
+#' @return A data.frame with "n", "prop", "pc", "labels" column
+#' @export
+#'
+#' @examples
+#' count_vars_plot(iris, Species)
+count_vars_plot <- function(x,
+                            ...,
+                            var_desc = TRUE,
+                            label_number = c("percent","count","both"),
+                            lump_prop = 0,
+                            other_level = "Other"
+) {
+
+  label_number <- match.arg(label_number)
+  vars <- dplyr::ensyms(..., .named = TRUE)
+
+  labels_expr <- switch (label_number,
+                         "percent" = { dplyr::expr(pc) },
+                         "count" = {dplyr::expr(n)},
+                         "both" = {dplyr::expr(glue::glue("{pc} ({n})"))}
+  )
+  ## Lump Factor
+  if (lump_prop != 0) {
+    x <- x %>%
+      dplyr::mutate(
+        dplyr::across(c(!!!vars), ~forcats::fct_lump(.x, prop = lump_prop,
+                                                     other_level = other_level)))
+
+  }
+
+  x %>%
+    dplyr::count(!!!vars, sort = TRUE) %>%
+    dplyr::mutate(prop = n/sum(n),
+                  percent = prop*100,
+                  pc = paste0(round(percent, 1),"%"),
+                  labels = !!labels_expr) %>%
+    dplyr::mutate(dplyr::across(c(!!!vars), ~forcats::fct_reorder(.x, prop, .desc = var_desc)))
+}
+
+
+
+# Count Variables to Plot Bar Graph with Facet ----------------------------
+
+
+
+#' Count Variables to Plot Bar Graph with Facet
+#'
+#' A Helper that count variable in each group of `var_facet` do extract percentage and construct `labels` column
+#' ready to be plotted.
+#'
+#' @param x A data.frame
+#' @param var (Quoted or Unquoted) Variable to count
+#' @param var_facet (Quoted or Unquoted) Variable intended to get facet wrapped (grouping variable)
+#' @param label_number Construction of `labels` column in the output
+#' * "percent": use `%` (eg. 60%)
+#' * "count": count as integer (eg. 6)
+#' * "both": use `%` and count in parenthesis  (eg. 60% (6))
+#'
+#' @return A data.frame
+#' @export
+#'
+#' @examples
+#' count_vars_plot_facet(mtcars, cyl, gear)
+count_vars_plot_facet <- function(x,
+                                  var,
+                                  var_facet,
+                                  label_number = c("percent","count","both")
+) {
+
+  label_number <- match.arg(label_number)
+  var <- dplyr::ensym(var)
+  var_facet <- dplyr::ensym(var_facet)
+
+  labels_expr <- switch (label_number,
+                         "percent" = { dplyr::expr(pc) },
+                         "count" = {dplyr::expr(n)},
+                         "both" = {dplyr::expr(glue::glue("{pc} ({n})"))}
+  )
+
+  x %>%
+    dplyr::count(!!var_facet, !!var) %>%
+    dplyr::group_by(!!var_facet) %>%
+    dplyr::mutate(prop = n/sum(n),
+                  percent = prop*100,
+                  pc = paste0(round(percent, 1),"%"),
+                  labels = !!labels_expr) %>%
+    dplyr::ungroup()
+}
+
+
