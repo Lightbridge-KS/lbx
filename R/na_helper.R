f954e2a222b32a8bd168a1e9a793d629313117a8
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-09-25 18:33:53 +0700

    Add fill_na()

---
+++ R/na_helper.R
@@ -0,0 +1,29 @@
+#' Fill Missing values from another vector
+#'
+#' Fill `NA`s values from `x` using values from `fill` that in the same location.
+#' Vector `x` and `fill` must have the same length.
+#'
+#' @param x Vector to fill `NA`
+#' @param fill A Vector whose elements will be retrieved to fill in `x`
+#'
+#' @return A Vector with original element from `x` and filled `NA` from `fill`
+#' @export
+#' @example
+#'
+#' v1 <- c(NA, NA, "a", NA, "b") # Trust v1
+#' v2 <- c(NA, "c", "d","k", NA)
+#' fill_na(v1, v2)
+#'
+fill_na <- function(x, fill) {
+
+  if(length(x) != length(fill)) stop("`x` and `fill` must have the same length", call. = F)
+
+  from_fill_lgl <- is.na(x) & !is.na(fill) # Select these element from fill
+  from_x_lgl <- !from_fill_lgl  # Select these element from x
+
+  z <- vector(class(c(x,fill)), length = length(x))
+  z[from_fill_lgl] <- fill[from_fill_lgl]
+  z[from_x_lgl] <- x[from_x_lgl]
+  z
+
+}
