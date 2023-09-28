9f6b7815198cb67365387415827210f8e2a9995b
Kittipos <ki11ip0.s.a.s@gmail.com>
2023-09-28 15:19:24 +0700

    add `complete_rate()`

--- R/na_helper.R
+++ R/na_helper.R
@@ -27,3 +27,29 @@ fill_na <- function(x, fill) {
   z
 
 }
+
+
+#' Calculate Completeness of Data
+#'
+#' Calculate proportion of completeness (no `NA`) of the element in the data frame or list
+#'
+#' @param x list or data.frame
+#' @param negate `TRUE` to return proportion of missing data
+#'
+#' @return A vector of proportion of completeness (not missing)
+#' @export
+#'
+#' @examples
+#' df <- data.frame(x = c(1, NA, 2),
+#'                  y = c(NA, "a", "b"),
+#'                  z = 1:3)
+#'
+#' complete_rate(df)
+complete_rate <- function(x,
+                          negate = FALSE
+) {
+  stopifnot(is.list(x))
+  res <- vapply(x, function(x) mean(!is.na(x)), numeric(1))
+  if(negate) return(1 - res)
+  res
+}
